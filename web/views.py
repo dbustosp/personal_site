bfda82de309f25cbf0f1a3d4fbf5a1cd60767fcb
dbustosp <dbustos10@gmail.com>
2014-10-18 20:59:02 -0300

    Agregado primera vista index a app web

--- web/views.py
+++ web/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.shortcuts import render_to_response
+from django.template import Context
+from django.template.context import RequestContext
 
-# Create your views here.
+
+def index(request):
+	return render_to_response(
+		'aboutme.html',
+		{},
+		context_instance=RequestContext(request)
+		)
