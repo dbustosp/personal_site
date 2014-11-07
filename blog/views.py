d412b1ce9e0c46fc3b65073f7ca6b5e36f257025
Danilo <dbustos10@gmail.com>
2014-11-07 01:54:57 -0300

    arreglado problema mimetype

--- blog/views.py
+++ blog/views.py
@@ -2,12 +2,11 @@ from django.shortcuts import render
 from django.shortcuts import render_to_response
 from django.template import Context
 from django.template.context import RequestContext
+from django.http import HttpResponse
+from django.template.response import TemplateResponse
 
 def index(request):
-	return render_to_response(
-		'index.html',
-		{},
-		context_instance=RequestContext(request)
-		)
+    print "hola"
+    return TemplateResponse(request, 'index.html', {})
 
 
--- web/views.py
+++ web/views.py
@@ -2,11 +2,9 @@ from django.shortcuts import render
 from django.shortcuts import render_to_response
 from django.template import Context
 from django.template.context import RequestContext
+from django.template.response import TemplateResponse
 
 
 def index(request):
-	return render_to_response(
-		'aboutme.html',
-		{},
-		context_instance=RequestContext(request)
-		)
+    print "hola"
+    return TemplateResponse(request, 'aboutme.html', {})
