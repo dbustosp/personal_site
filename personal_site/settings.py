7a941ee742461b5fe10c51a0d90b5a98b14be91d
dbustosp <dbustos10@gmail.com>
2014-11-13 18:22:31 -0300

    updated settings.py

--- personal_site/settings.py
+++ personal_site/settings.py
@@ -1,23 +1,6 @@
-
-
-import dj_database_url
-#DATABASES['default'] =  dj_database_url.config()
-
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
-ALLOWED_HOSTS = ['*']
-
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-# URL static files
-STATIC_ROOT = "/app/static/"
-STATIC_URL = '/static/'
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
 
 PROJECT_DIR = os.path.dirname(__file__).decode('utf-8')
 MEDIA_ROOT = os.path.join(PROJECT_DIR, 'media/')
@@ -35,6 +18,9 @@ ADMINS = (
 
 
 TEMPLATE_DEBUG = True
+
+ALLOWED_HOSTS = ['*']
+
 
 # Application definition
 
@@ -66,18 +52,6 @@ WSGI_APPLICATION = 'personal_site.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
-DATABASES = {
-     'default': {
-         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-         'NAME': 'd3fm0u67tb6ndl',                      # Or path to database file if using sqlite3.
-         'USER': 'zsnsvplamuwjsq',                      # Not used with sqlite3.
-         'PASSWORD': '947wY90OmoDAoM0ltFwzsropSU',                  # Not used with sqlite3.
-         'HOST': 'ec2-54-83-33-14.compute-1.amazonaws.com',                      # Set to empty string for localhost. Not used with sqlite3.
-         'PORT': '5432',                      # Set to empty string for default. Not used with sqlite3.
-     }
-}
-
-
 """
 DATABASES = {
      'default': {
@@ -91,6 +65,16 @@ DATABASES = {
 }
 """
 
+DATABASES = {
+     'default': {
+         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+         'NAME': 'd3fm0u67tb6ndl',                      # Or path to database file if using sqlite3.
+         'USER': 'zsnsvplamuwjsq',                      # Not used with sqlite3.
+         'PASSWORD': '947wY90OmoDAoM0ltFwzsropSU',                  # Not used with sqlite3.
+         'HOST': 'ec2-54-83-33-14.compute-1.amazonaws.com',                      # Set to empty string for localhost. Not used with sqlite3.
+         'PORT': '5432',                      # Set to empty string for default. Not used with sqlite3.
+     }
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
@@ -137,4 +121,7 @@ TEMPLATE_DIRS = (
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
+# URL static files
+STATIC_ROOT = "/home/dbustos/personal_site/static/"
 
+STATIC_URL = '/static/'
